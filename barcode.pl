c02f64c9741d137ea0baa98e1388180308775475
Juerd Waalboer <juerd@tnx.nl>
2017-05-07 00:19:57 +0200

    Multiple barcode labels at once

--- barcode.pl
+++ barcode.pl
@@ -5,20 +5,26 @@ use Barcode::Code128;
 use File::Temp qw(tempfile);
 
 my $b = Barcode::Code128->new;
-$b->option(scale => 1);
+$b->option(scale => 2);
 $b->option(height => 64);
 $b->option(border => 0);
 $b->option(font => 'giant');
 $b->option(font_align => 'center');
 $b->option(font_margin => 0);
 
-my ($fh, $fn) = tempfile;
+my @bitmaps;
 
-open my $pipe, "|-", qw[convert PNG:- -gravity center -extent x128
-    -background white], "PBM:$fn";
-print $pipe $b->png(shift);
-close $pipe;
+while (@ARGV) {
+    my ($fh, $fn) = tempfile;
 
-system "./ptouch-770-write", $fn;
+    open my $pipe, "|-", qw[convert PNG:- -gravity center -extent x128
+        -background white], "PBM:$fn";
+    print $pipe $b->png(shift);
+    close $pipe;
 
-unlink $fn;
+    push @bitmaps, $fn;
+}
+
+system "./ptouch-770-write", @bitmaps;
+
+unlink @bitmaps;
